5c37faad5d456cb141175e8628f3fed61ef9bbc8
nrana-pixel <nrana4148@gmail.com>
2025-07-05 00:18:32 +0530

    models and routes created

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 const express = require('express')
 const app = express();
+const cookieparser = require('cookie-parser');
+const path = require('path');
+const db = require("./config/mongoose-connection");
+
+const userModel = require("./models/user-model");
+const ownerModel = require("./models/owner-model");
+const productModel = require("./models/product-model");
+
+const ownersRouter = require("./routes/ownersRouter");
+const usersRouter = require("./routes/usersRouter");
+const productRouter = require("./routes/productsRouter");
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieparser());
+app.use(express.static(path.join(__dirname,"public")));
+app.set("view engine",'ejs');
+
+app.use("/owners",ownersRouter);
+app.use("/products",productRouter);
+app.use("/users",usersRouter);
 
 app.get("/",function(req,res){
     res.send("hello");
---
+++ models/owner-model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const ownerSchema = mongoose.Schema({
+    fullname: {
+        type: String,
+        minLength: 3,
+        trim: true
+    },
+    email: String,
+    password: String,
+    picture: String,
+    gstin: String,
+    products:{
+        type: Array,
+        default: []
+    }
+})
+
+module.exports = mongoose.model("owner",ownerSchema)
